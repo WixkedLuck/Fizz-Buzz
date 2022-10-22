087a55b632b4b4080dd594cb32996aef9b3be1e2
David Hybner <dah1998@yahoo.com>
2022-10-22 11:35:31 -0400

    update app.js
    added a function to determine if a users numbers input is fizz, buzz , or FizzBuzz. Output is then displayed in the array

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # Fizz-Buzz
-A front end application that teaches division
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributors](#Contributors)
+
+## Description:
+
+- FizzBuzz is a very simple programming task, asked in software development jobs. The purpose of this game is to teach division using two starting numbers. Whenever a number is divisable buy one or both numbers, the word changes to Fizz, Buzz or FizzBuzz in its place. With the use of react and state change, the page will dynamically update everytime an input has changed to reflect the current numbers selected for FizzBuzz. 
+
+## Installation
+
+1. `git clone WixkedLuck/Fizz-Buzz`
+2. run `npm start`
+
+## Usage
+
+- The purpose of this app is to show how to solve the programming task of FizzBuzz in react. 
+
+GitHub URL: https://github.com/WixkedLuck/Fizz-Buzz
+
+GitHub Pages: https://backstock-23.herokuapp.com/
+
+## License
+
+- N/A<br/>
+
+## Contributors
+
+- WixkedLuck
+
+
+## Test
+
+- N/A <br/>
+_Screenshots of page_
+![FizzBuzz](./my-app/public/images/FizzBuzz.PNG)
+
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,11 +5,22 @@ function App() {
   const [firstNum, setFirstnum] = useState(3);
   const [secondNum, setsecondnum] = useState(5);
   console.log(firstNum);
-  return (
+  
+  function FizzBuzz(i){
     
+    if (i%firstNum===0 && i%secondNum===0)
+    {return "FizzBuzz"}
+    else if (i%firstNum===0)
+    {return "Fizz" }
+    else if(i%secondNum===0)
+    {return "Buzz"}
+    return i;
+  }
+  return (
+
     <div className="App">
-     
-      <h1 className='animate__animated animate__zoomIn title'>Fizz Buzz is a very simple programming task, asked in software developer job interviews...</h1>
+
+      <h1 className='animate__animated animate__zoomIn title'><span className="Fizz">Fizz</span><span className="Buzz">Buzz</span> is a very simple programming task, asked in software developer job interviews...</h1>
       <div className="headers">
         <h1 className="Fizz animate__animated animate__backInDown animate__delay-5s">Fizz</h1>
         <h1 className="Buzz animate__animated animate__backInUp animate__delay-5s ">Buzz</h1>
@@ -17,25 +28,23 @@ function App() {
       </div>
 
       <div className="textinput">
-        <input className="animate__animated animate__bounceIn animate__delay-5s" type="number" id="fnum" value={firstNum} onChange={e=>setFirstnum(e.target.value)}></input>
-        <input className="animate__animated animate__bounceIn animate__delay-5s" type="number" id="secnum" value={secondNum} onChange={e=>setsecondnum(e.target.value)} ></input>
+        <input className="animate__animated animate__bounceIn animate__delay-5s" type="number" id="fnum" min="1" max="100" value={firstNum} onChange={e => setFirstnum(e.target.value)}></input>
+        <input className="animate__animated animate__bounceIn animate__delay-5s" type="number" min="1" max="100" id="secnum" value={secondNum} onChange={e => setsecondnum(e.target.value)} ></input>
         {/* <button type="button" id="submit">Submit</button> */}
       </div>
 
       <div className="numbers">
-      <table>
-  <tr className='animate__animated animate__rollIn animate__delay-2s'>
-   
-    {Array.from(Array(100)).map((num, i) => <th>{i + 1}</th>)}
-  
-
-  </tr>
-  </table>
+        <table>
+          <tr className='animate__animated animate__rollIn animate__delay-2s'>
+            {Array.from(Array(100)).map((num, i) => <th key={i}>{FizzBuzz(i+1) }</th>)}
+           
+          </tr>
+        </table>
       </div>
-      
+
     </div>
-    
-    
+
+
   );
 }
 
